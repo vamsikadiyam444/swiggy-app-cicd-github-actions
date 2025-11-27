8d4742df766238fcb9ed38dd89b4e44a748a68c6
vamsikadiyam444 <vamsikadiyam444@gmail.com>
2025-11-27 13:41:03 -0500

    Refactor App component with React Router

--- App.js
+++ App.js
@@ -1,74 +1,26 @@
-import React, { useEffect, useState } from "react";
-import ReactDOM from "react-dom/client"
-import Header from "./src/compnents/Header";
-import Body from "./src/compnents/Body";
-import { createBrowserRouter , RouterProvider , Outlet, Form} from "react-router-dom";
-import About from "./src/compnents/About";
-import ContactUs from "./src/compnents/ContactUs";
-import Error from "./src/compnents/Error";
-import RestaurantMenu from "./src/compnents/RestaurantMenu";
-import UserContext from "./utils/UserContext";
-import { Provider } from "react-redux";
-import appStore from "./utils/appStore";
-import Cart from "./src/compnents/Cart";
+import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 
+// Import your components
+import Home from "./components/Home";
+import Menu from "./components/Menu";
+import Cart from "./components/Cart";
+import About from "./components/About";
+import NotFound from "./components/NotFound"; // Optional 404 page
 
-const App = () => {
-    const [userName , setUserName] = useState("")
-
-    useEffect(()=>{
-        // make an api call and get the userinfo
-        const data = {
-            name:'Chakri seelam',
-        }
-        setUserName(data.name)
-    },[])
-    return(
-        <Provider store={appStore}>
-        <UserContext.Provider value={{loggedInUser:userName , setUserName}}>
-        <>
-        <Header />
-        <Outlet />
-
-        </>
-        </UserContext.Provider>
-        </Provider>
-    )
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/about" element={<About />} />
+        {/* Catch-all route for unmatched URLs */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
-const appRouter = createBrowserRouter([
-    {
-        
-        element:<App />,
-        children : [
-            {
-        path: "/",
-        element:<Body />,
-    },
-              
-    {
-        path: "/about",
-        element:<About />,
-    },
-    {
-        path: "/contact",
-        element:<ContactUs />,
-    },
-    {
-        path: "/resmenu/:resId",
-        element:<RestaurantMenu />,
-    },{
-
-        path:"/cart",
-        element:<Cart />
-
-    },
-
-        ],
-        errorElement:<Error />
-}])
-
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-<RouterProvider router= {appRouter}/>);
+export default App;
