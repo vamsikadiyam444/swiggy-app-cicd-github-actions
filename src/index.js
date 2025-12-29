c872ac2200b9cd0d9517a629e773f1e0aadd5ab9
vamsikadiyam444 <vamsikadiyam444@gmail.com>
2025-12-29 18:31:02 -0500

    trigger cicd.yml

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React fromi "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";  // Path to your App.js
+import { HashRouter as Router } from "react-router-dom";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <Router>
+        <App />
+    </Router>
+);
+
